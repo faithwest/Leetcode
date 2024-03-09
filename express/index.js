bfbfa272dcfa749371b8a69482df2baef1183a69
faithwest <faithchepkoech425@gmail.com>
2024-03-09 17:10:33 +0300

    creating tables with express js

---
+++ express/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+ app.get('/', (req, res) => {
+    console.log ('[TEST]!');
+    res.send('Welcome to Home Page.');
+ });
+
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
