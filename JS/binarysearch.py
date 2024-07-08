f30ad7ca0bc396d361b316012e17a46f2a50d2e2
faithwest <faithchepkoech425@gmail.com>
2024-07-08 23:12:14 +0300

    linked list problem

--- JS/binarysearch.py
+++ JS/binarysearch.py
@@ -17,3 +17,30 @@ def binary_search(arr, target):
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target = 7
 print(binary_search(arr, target))  # Output: 6
+
+##################################################
+##linked list
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def find_middle(head):
+    slow = fast = head
+    
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    
+    return slow
+
+# Example usage:
+# Creating a linked list: 1 -> 2 -> 3 -> 4 -> 5
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+middle_node = find_middle(head)
+print(middle_node.val)  # Output: 3
