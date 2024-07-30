5cbd606e7ab3696dcea733548351aa681384def3
faithwest <faithchepkoech425@gmail.com>
2024-07-30 11:19:06 +0300

    substring answer

--- JS/substring.py
+++ JS/substring.py
@@ -1,3 +1,16 @@
 # Input: s = "abcabcbb"
 # Output: 3
 # Explanation: The answer is "abc", with the length of 3.
+def length_of_longest_substring(s: str) -> int:
+    char_set = set()
+    left = 0
+    max_length = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_length = max(max_length, right - left + 1)
+    
+    return max_length
