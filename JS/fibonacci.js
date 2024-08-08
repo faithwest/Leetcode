1ad48c7376b48395416e23acb3af5fd17c72caf0
faithwest <faithchepkoech425@gmail.com>
2024-08-08 10:10:09 +0300

    flattenarray

--- JS/fibonacci.js
+++ JS/fibonacci.js
@@ -6,3 +6,8 @@ function fibonacci(n) {
     return seq;
 }
 console.log(fibonacci(10)); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+function flattenArray(arr) {
+    return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
+}
+console.log(flattenArray([1, [2, [3, [4, 5]]]])); // Output: [1, 2, 3, 4, 5]
