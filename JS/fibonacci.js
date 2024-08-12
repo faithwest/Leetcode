db1cb18b20e48b2036d8682995cfe7a4065ff568
faithwest <faithchepkoech425@gmail.com>
2024-08-12 09:09:20 +0300

    learning integers

--- JS/fibonacci.js
+++ JS/fibonacci.js
@@ -1,16 +1,16 @@
-function fibonacci(n) {
-    const seq = [0, 1];
-    for (let i = 2; i < n; i++) {
-        seq.push(seq[i - 1] + seq[i - 2]);
-    }
-    return seq;
-}
-console.log(fibonacci(10)); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// function fibonacci(n) {
+//     const seq = [0, 1];
+//     for (let i = 2; i < n; i++) {
+//         seq.push(seq[i - 1] + seq[i - 2]);
+//     }
+//     return seq;
+// }
+// console.log(fibonacci(10)); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-function flattenArray(arr) {
-    return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
-}
-console.log(flattenArray([1, [2, [3, [4, 5]]]])); // Output: [1, 2, 3, 4, 5]
+// function flattenArray(arr) {
+//     return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
+// }
+// console.log(flattenArray([1, [2, [3, [4, 5]]]])); // Output: [1, 2, 3, 4, 5]
 def product_except_self(nums):
     length = len(nums)
     answer = [0] * length
---
+++ JS/integers.py
@@ -0,0 +1,18 @@
+# Assigning integer values to variables
+positive_integer = 42
+negative_integer = -17
+zero = 0
+
+# Performing basic arithmetic operations with integers
+sum_result = positive_integer + negative_integer  # 42 + (-17) = 25
+difference_result = positive_integer - negative_integer  # 42 - (-17) = 59
+multiplication_result = positive_integer * 2  # 42 * 2 = 84
+division_result = positive_integer // 5  # Integer division: 42 // 5 = 8
+modulus_result = positive_integer % 5  # Remainder: 42 % 5 = 2
+
+# Printing the results
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Multiplication:", multiplication_result)
+print("Integer Division:", division_result)
+print("Modulus:", modulus_result)
