79325f3799381c9814e7c4a5c70a864e9fb9e993
faithwest <faithchepkoech425@gmail.com>
2024-08-14 21:43:44 +0300

    usestate hook

--- JS/fibonacci.js
+++ JS/fibonacci.js
@@ -1,34 +1,42 @@
-// function fibonacci(n) {
-//     const seq = [0, 1];
-//     for (let i = 2; i < n; i++) {
-//         seq.push(seq[i - 1] + seq[i - 2]);
-//     }
-//     return seq;
-// }
-// console.log(fibonacci(10)); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// // function fibonacci(n) {
+// //     const seq = [0, 1];
+// //     for (let i = 2; i < n; i++) {
+// //         seq.push(seq[i - 1] + seq[i - 2]);
+// //     }
+// //     return seq;
+// // }
+// // console.log(fibonacci(10)); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-// function flattenArray(arr) {
-//     return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
-// }
-// console.log(flattenArray([1, [2, [3, [4, 5]]]])); // Output: [1, 2, 3, 4, 5]
-def product_except_self(nums):
-    length = len(nums)
-    answer = [0] * length
+// // function flattenArray(arr) {
+// //     return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
+// // }
+// // console.log(flattenArray([1, [2, [3, [4, 5]]]])); // Output: [1, 2, 3, 4, 5]
+// def product_except_self(nums):
+//     length = len(nums)
+//     answer = [0] * length
 
-    answer[0] = 1
-    for i in range(1, length):
-        answer[i] = nums[i - 1] * answer[i - 1]
+//     answer[0] = 1
+//     for i in range(1, length):
+//         answer[i] = nums[i - 1] * answer[i - 1]
 
-    right = 1
-    for i in reversed(range(length)):
-        answer[i] = answer[i] * right
-        right *= nums[i]
+//     right = 1
+//     for i in reversed(range(length)):
+//         answer[i] = answer[i] * right
+//         right *= nums[i]
 
-    return answer
+//     return answer
 
-# Example usage
-nums = [1,2,3,4]
-print(product_except_self(nums))  # Output: [24,12,8,6]
+// # Example usage
+// nums = [1,2,3,4]
+// print(product_except_self(nums))  # Output: [24,12,8,6]
 
-nums = [-1,1,0,-3,3]
-print(product_except_self(nums))  # Output: [0,0,9,0,0]
+// nums = [-1,1,0,-3,3]
+// print(product_except_self(nums))  # Output: [0,0,9,0,0]
+useEffect(() => {
+    const timer = setInterval(() => {
+      console.log('This runs every second');
+    }, 1000);
+  
+    return () => clearInterval(timer); // Cleanup function
+  }, []);
+  
